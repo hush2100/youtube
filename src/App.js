4255582ddb9fbccd81fc4e0673e1f66e992d5302
hush2100 <hush2100@coremethod.co.kr>
2023-11-01 14:55:24 +0900

    header작업

--- src/App.js
+++ src/App.js
@@ -14,23 +14,32 @@ import Video from "./pages/Video";
 import Search from "./pages/Search";
 import Not from "./pages/Not";
 
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/today" element={<Today />} />
-        <Route path="/developer" element={<Developer />} />
-        <Route path="/webd" element={<Webd />} />
-        <Route path="/website" element={<Website />} />
-        <Route path="/gsap" element={<Gsap />} />
-        <Route path="/port" element={<Port />} />
-        <Route path="/youtube" element={<Youtube />} />
-        <Route path="/channel" element={<Channel />} />
-        <Route path="/video" element={<Video />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="*" element={<Not />} />
-      </Routes>
+      <Header />
+      <Main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/today" element={<Today />} />
+          <Route path="/developer" element={<Developer />} />
+          <Route path="/webd" element={<Webd />} />
+          <Route path="/website" element={<Website />} />
+          <Route path="/gsap" element={<Gsap />} />
+          <Route path="/port" element={<Port />} />
+          <Route path="/youtube" element={<Youtube />} />
+          <Route path="/channel/:channelId" element={<Channel />} />
+          <Route path="/video/:videoId" element={<Video />} />
+          <Route path="/search/:searchId" element={<Search />} />
+          <Route path="*" element={<Not />} />
+        </Routes>
+      </Main>
+      <Footer />
     </BrowserRouter>
   );
 }
