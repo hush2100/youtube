81fe4d74c9e4024be02e17f0953842e17fa257b8
hush2100 <hush2100@coremethod.co.kr>
2023-11-01 16:37:49 +0900

    컴퍼넌트비동기작업

---
+++ src/components/section/Main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Main = (props) => {
+  return(
+    <>
+      <Header />
+      <main id="main" role="main">
+        {props.children}
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default Main;
