35bf69e56dfc0765ddcdc5a4dd0f251a87ca0805
hush2100 <hush2100@coremethod.co.kr>
2023-11-03 11:25:44 +0900

    search작업

--- src/components/section/Search.jsx
+++ src/components/section/Search.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Search = () => {
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSearch = () => {
+    console.log(searchKeyword);
+    if(searchKeyword){
+      navigate(`/search/${searchKeyword}`);
+      setSearchKeyword('');
+    }
+  }
+
   return (
     <div id="search">
       <div className="search__inner">
         <label htmlFor="searchInput">
           <span className="ir">검색</span>
         </label>
-        <input type="search" id="searchInput" placeholder="검색어를 입력하세요" className="search__input" autoComplete="off"  />
+        <input type="search" id="searchInput" placeholder="검색어를 입력하세요" className="search__input" autoComplete="off" 
+        onChange={e => setSearchKeyword(e.target.value)}
+        onKeyDown={e => {
+          if(e.key === 'Enter'){
+            handleSearch();
+          }
+        }}
+        />
       </div>
     </div>
   );
